c04ed4565946eb004901ee52366f89442b43fa60
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2020-10-27 20:26:17 +0100

    zacatek nocni mury
    ty ukoly jsou posledni dobou fakt scam. kámoš chodí na státní střední a už vydal na google playi hru #thisisnotwhatmyparentspayfor

---
+++ src/map.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class map {
+    public static void create(int size, int cars, int bats, int dests, int subs, int pats){
+        tileType[][] mapka = new tileType[size][size];
+    }
+}
